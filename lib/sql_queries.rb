461083aa35231fb64c222f24e44b7518d7ce779d
Micah Bowie <micahbowie20@gmail.com>
2020-08-25 02:34:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,8 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, pledges.amount 
   FROM pledges 
   INNER JOIN users
-  ON pledges.user_id = users.id"
+  ON pledges.user_id = users.id
+  ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
