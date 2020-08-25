8be845423940611e7c66e180120bedbcb9367f8b
Micah Bowie <micahbowie20@gmail.com>
2020-08-25 02:12:57 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -13,3 +13,12 @@ id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER
 );
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date INTEGER,
+end_date INTEGER, 
+);
