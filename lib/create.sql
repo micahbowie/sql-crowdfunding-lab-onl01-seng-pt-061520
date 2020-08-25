fe5be5708682d21f858778b2a8df0ce19c25ca1a
Micah Bowie <micahbowie20@gmail.com>
2020-08-25 02:12:45 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,5 +4,12 @@ title TEXT,
 category TEXT,
 funding_goal INTEGER,
 start_date INTEGER,
-end_date INTEGER, 
+end_date INTEGER 
+);
+
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
 );
